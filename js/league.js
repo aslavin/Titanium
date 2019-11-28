1bae4056866102374bb681805b8cef1200af1c3f
tclare <tclare@nd.edu>
2019-11-28 00:53:07 -0500

    frontend thanksgiving; pool, league, login, user frontend done

--- js/league.js
+++ js/league.js
@@ -1,25 +1,54 @@
-var leagueData = [["","Sun","7:00p"],["","Sun","8:00p"],["","Mon","7:00p"],["","Tues","8:00p"],["","Wed","2:30p"]];
+// TODO: fetch league data (i.e., pools) and store it in variable leagueData in the format as follows: 
+// POOL LINK - POOL DAY - POOL TIME - POOL # TEAM SPOTS AVAILABLE - POOL TEAM CAPACITY. 
+// Example is below
+var leagueData = [["","Sun","7:00p", "1","5"],["","Sun","8:00p", "3","5"],["","Mon","7:00p", "0","5"],["","Tues","8:00p", "0","5"],["","Wed","2:30p", "0","5"]];
 
 var leagueLeagueComponents = ['<div class="col-6 col-md-4 col-lg-3 leaguePoolCol"><a class="imageLink" href="',
-    '"><div class="leaguePool"><span class="leagueDescription">',
+    '"><div class="leaguePool">',
+    '<span class="leagueDescription">',
     '</span><span class="leagueDescription">',
     '</span></div></a></div>'
 ];
 
-function loadData() { 
-    for (var i = 0; i < leagueData.length; i++) { 
-        document.getElementById("leaguePools").innerHTML += leagueLeagueComponents[0] + leagueData[i][0] + leagueLeagueComponents[1] + leagueData[i][1] + leagueLeagueComponents[2] + leagueData[i][2] + leagueLeagueComponents[3];
+function loadData() {
+
+   /* EXECUTE TODO ITEMS HERE: connect to backend, manipulate global leagueData variable */ 
+
+   /* DONE: SET LEAGUE STATISTICS */
+    
+    document.getElementById("totalPools").innerHTML = leagueData.length;
+    var nPoolsAvailable = 0;
+    var nTeamsAvailable = 0;
+    var nTeams = 0;
+
+    for (var a = 0; a < leagueData.length; a++) { 
+        if (leagueData[a][3] > 0) {
+            nTeamsAvailable += parseInt(leagueData[a][3]);
+            nPoolsAvailable++;
+        }
+        nTeams += parseInt(leagueData[a][4]);
+    }
+
+    document.getElementById("poolsAvailable").innerHTML = nPoolsAvailable;
+    document.getElementById("teamCapacity").innerHTML = nTeams;
+    document.getElementById("teamsAvailable").innerHTML = nTeamsAvailable; 
+
+    /* DONE: DISPLAY CONTENT RELEVANT TO POOL DATA */
+
+    for (var i = 0; i < leagueData.length; i++) {
+    
+        var fullMessage = (leagueData[i][3] == 0) ? '<div class="leagueFullBanner">FULL</div>': '';
+
+        document.getElementById("leaguePools").innerHTML += leagueLeagueComponents[0] + leagueData[i][0] + leagueLeagueComponents[1] + fullMessage + leagueLeagueComponents[2] + leagueData[i][1] + leagueLeagueComponents[3] + leagueData[i][2] + leagueLeagueComponents[4];
     }
 
     var nPoolsAvailable = document.getElementById("poolsAvailable").innerHTML;
     if (nPoolsAvailable == 0) document.getElementById("noMoreSpaceAlert").style.display = "block";
-   
+  
+    /* DONE: COMPUTE FOOTER MARGIN */
+
     var ch = document.getElementById("landingFooterUser").clientHeight;
     var ih = window.innerHeight;
     var sh = document.getElementById("mainContent").scrollHeight;
-    if (ih - ch - sh > 0) {
-        document.getElementById("landingFooterUser").style.marginTop = (ih - ch - sh) + "px"; 
-        console.log(document.getElementById("landingFooterUser").style.marginTop);
-    } 
-
+    if (ih - ch - sh > 0) document.getElementById("landingFooterUser").style.marginTop = (ih - ch - sh) + "px"; 
 }
--- js/pool.js
+++ js/pool.js
@@ -1,16 +1,23 @@
-/* the following array is assumed to be in RANK order */
-
-var poolData = [["","Boolin Boys","3","0"],["","Papal Rage","2","1"],["","Broom Roasted","1","2"],["","Stix Or It Didn't Happen","0","3"],["","Another Team", "0", "4"]]; // indexed by LINK - TEAM - TEAM WINS - TEAM LOSSES
-
+// TODO: fetch pool data from database and store it in var called poolData with this format: TEAM LINK - TEAM NAME - TEAM WINS - TEAM LOSSES. 
+// NOTE that it is assumed that the array poolData has been sorted in RANK ORDER.
+// Example is here:
+var poolData = [["","Andy's Army","4","0"],["","Broom Roasted","3","1"],["","Stix Or It Didn't Happen","2","2"],["","Another Team 1","1","3"]]; 
 function loadData() { 
 
-    document.getElementById("teamsRegistered").innerHTML = poolData.length;
-    
+    /* EXECUTE TODO items here; connect to backend, manipulate poolData variable as above.
+        
+    ALSO TODO:  set innerHTML of #poolCapacity, set innerHTML of #playoffEligible */
+
+    document.getElementById("teamsRegistered").innerHTML = poolData.length;  
+
+    /* DONE: FILL OUT THE TABLE OF POOL DATA */
+
     if (poolData.length == 0) { 
         document.getElementById("noTeamsInPool").style.display = "block";
+        document.getElementById("actualPoolTable").style.display = "none";
+        document.getElementById("poolSignUp").style.display = "block";
     } else { 
-        var poolTable = document.getElementById("actualPoolTable"); 
-        
+        var poolTable = document.getElementById("actualPoolTable");  
         for (var i = 0; i < poolData.length; i++) {
             var row = poolTable.insertRow(i+1);
             var rank = row.insertCell(0);
@@ -21,16 +28,118 @@ function loadData() {
             record.innerHTML = '<span id="nWins' + (i+1) + '">' + poolData[i][2] + '</span> - <span id="nLosses' + (i+1) + '">' + poolData[i][3] + '</span></td></tr>';
         }
 
+        /* DONE: DETERMINE WHETHER A USER SHOULD BE ABLE TO REGISTER A NEW TEAM */
+        
         var teamsRegistered = document.getElementById("teamsRegistered").innerHTML;
-        var teamCapacity = document.getElementById("teamCapacity").innerHTML;
+        var teamCapacity = document.getElementById("poolCapacity").innerHTML;
 
         if (teamCapacity == teamsRegistered) document.getElementById("noSpaceInPoolAlert").style.display = "block";
         else  document.getElementById("poolSignUp").style.display = "block";     
     }
+
+    /* DONE: COMPUTE FOOTER POSITION */
 
     var ch = document.getElementById("landingFooterUser").clientHeight;
     var ih = window.innerHeight;
     var sh = document.getElementById("mainContent").scrollHeight;
     if (ih - ch - sh > 0) document.getElementById("landingFooterUser").style.marginTop = (ih - ch - sh) + "px";
 
+}
+
+
+function clearAllAlerts() {
+    var alerts = document.getElementsByClassName("alert");
+    for (var i = 0; i < alerts.length; i++) { 
+        alerts[i].style.display = "none";
+    }   
+}
+
+/* DONE: FUNCTIONS CONCERNING THE MODAL */
+
+
+var nMembers = 0;
+var nMembersToCheck = 0;
+
+function readyModal() { 
+    nMembers = 0;
+    document.getElementById("usersToInvite").innerHTML = "";
+    document.getElementById("teamName").value = "";
+}
+
+function removeTeamMember(n) { 
+    document.getElementById("teamMemberWrapper" + n).remove(); /* yikes and a half */ 
+    nMembersToCheck--;
+}
+
+function addTeamMemberField()  {
+    nMembers++; nMembersToCheck++;
+    document.getElementById("usersToInvite").insertAdjacentHTML("beforeend", '<div class="teamMemberWrapper" id="teamMemberWrapper' + nMembers + '"><label for="teamMember' + nMembers + '">New Member <button type="button" class="btn btn-danger removeTeamMember" onclick="removeTeamMember(' + nMembers + ')"><i class="fa fa-close"></i>REMOVE</button></label><input type="text" class="form-control teamMemberText" placeholder="Email" id="teamMember' + nMembers + '"></div>');
+}
+
+function activateSingleAlert(alertId) {
+    
+    var alerts = document.getElementsByClassName("alert");
+    var alertToDelete = 0;
+    for (var i = 0; i < alerts.length; i++) {
+        if (alerts[i].id == alertId) {
+            alerts[i].style.display = "block";
+            alertToDelete = i;
+        }
+        else alerts[i].style.display = "none";
+    }
+}
+
+
+// Check each user invited, one by one, to see if they are suitable for invitation to the team.
+function processTeam() { 
+
+    clearAllAlerts();
+
+    var finalTeamName = document.getElementById("teamName").value;
+    if (finalTeamName.length == 0) { 
+        activateSingleAlert("absentTeamName");
+        return;
+    }  
+    /* TODO: check if the same team name already exists in the league. If so, call activateSingleAlert("teamNameAlreadyExists") and RETURN!*/
+    var finalTeamMembers = document.getElementsByClassName("teamMemberText"); 
+
+    // Loop through every team member entered: 
+    for (var i = 0; i < finalTeamMembers.length; i++) {            
+            /* - DONE: raise an alert if any email field is blank */
+            if (finalTeamMembers[i].value.length == 0) { 
+                activateSingleAlert("blankUserEmail");
+                return;
+            } 
+           
+            /*
+            - TODO: user email does not exist in the users db -- use activateSingleAlert("invalidTeamMemberEmail"), and set #invalidTeamMemberEmailText to finalTeamMembers[i].value. Then RETURN! See code below
+
+            activateSingleAlert("invalidTeamMemberEmail");
+            document.getElementById("invalidTeamMemberEmailText").innerHTML = finalTeamMembers[i].value;
+            return;*/
+
+            /*
+            - TODO: user (derived from email) is already on the team -- use activateSingleAlert("teamMemberAlreadyExists") and set #teamMemberAlreadyExists to finalTeamMembers[i].value. Then RETURN! See the code below.
+
+            activateSingleAlert("teamMemberAlreadyExists");
+            document.getElementById("teamMemberAlreadyExistsText").innerHTML = finalTeamMembers[i].value;
+            return;
+            */
+    }
+
+    createTeamAndSendInvitations();
+}
+
+
+function createTeamAndSendInvitations() {
+
+    /* After error checking all team members above, we can now create a new team, and send invitations to
+    // all the users that were invited.
+
+    /* TODO: create a new team */
+
+    var finalTeamMembers = document.getElementsByClassName("teamMemberText");
+    for (var i = 0; i < finalTeamMembers.length; i++) {
+        /* TODO: add each user (email found with finalTeamMembers[i].value) to the team you just made. */
+    }
 }
